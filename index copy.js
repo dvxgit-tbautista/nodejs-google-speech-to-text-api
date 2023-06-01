2f0d4d45698e9af4421f1bcd680b11946e06476d
Trixie Diane Bautista <tbautista@diavox.net>
2023-06-01 17:04:20 +0800

    Transcribing audio from your local folder

--- index copy.js
+++ index copy.js
@@ -1,26 +1,23 @@
-// Imports the required modules
-const fs = require('fs');
+// Imports the Google Cloud client library
 const speech = require('@google-cloud/speech');
 
 // Creates a client
 const client = new speech.SpeechClient();
 
 async function quickstart() {
-  // The path to the local audio file
-  const filePath = 'C:/put_your_local_folder_here/tongue twister 1.mp3';
-
-  // Read the audio file
-  const file = fs.readFileSync(filePath);
-  const audioBytes = file.toString('base64');
+  // The path to the remote LINEAR16 file
+//   const gcsUri = 'gs://cloud-samples-data/speech/brooklyn_bridge.raw';
+    const gcsUri = 'gs://diavox-project-1/transcripts/josie1.mp3';
 
   // The audio file's encoding, sample rate in hertz, and BCP-47 language code
   const audio = {
-    content: audioBytes,
+    uri: gcsUri,
   };
   const config = {
     encoding: 'MP3',
     sampleRateHertz: 48000,
-    languageCode: 'en-US',
+    // languageCode: 'en-US',
+    languageCode: 'fil-PH',
   };
   const request = {
     audio: audio,
@@ -34,5 +31,4 @@ async function quickstart() {
     .join('\n');
   console.log(`Transcription: ${transcription}`);
 }
-
 quickstart();
